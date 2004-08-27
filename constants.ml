ec80b01036bc2a7777308360c9e73ca47b889d25
Paul Pelzl <pelzlpj@eecs.umich.edu>
2004-08-27 06:58:16 +0000

    redefined constants using unit_of_string

--- constants.ml
+++ constants.ml
@@ -1,40 +1,9 @@
 open Units;;
 
-let electron_charge = {
-   coeff = 1.60217653e-19; 
-   factors = [ {
-      factor = Composite (NoPrefix, Coulomb);
-      power     = 1.0
-   } ]
-};;
-
-let electron_mass = {
-   coeff = 9.1093826e-31;
-   factors = [ {
-      factor = Mass (Kilo, Gram);
-      power     = 1.0
-   } ]
-};;
-
-let gravitation = {
-   coeff = 6.6742e-11;
-   factors = [ 
-      {factor = Distance (NoPrefix, Meter); power = 3.0};
-      {factor = Mass (Kilo, Gram); power = ~-. 1.0};
-      {factor = Time (NoPrefix, Second); power = ~-. 2.0}
-   ]
-};;
-
-let acceleration_gravity = {
-   coeff = 9.80665;
-   factors = [
-      {factor = Distance (NoPrefix, Meter); power = 1.0};
-      {factor = Time (NoPrefix, Second); power = ~-. 2.0}
-   ]
-};;
-
-
-
+let electron_charge      = unit_of_float_string 1.60217653e-19 "C"
+let electron_mass        = unit_of_float_string 9.1093826e-31 "kg"
+let gravitation          = unit_of_float_string 6.6742e-11 "m^3*kg^-1*s^-2"
+let acceleration_gravity = unit_of_float_string 9.80665 "m*s^-2"
 
 
 (* arch-tag: DO_NOT_CHANGE_1c6c88d4-3b57-4b5a-85fc-02372437ea3f *)
